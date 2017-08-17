ce747469d59ebc979dc6728a86c6fe14951060a6
JeffHykin <jeff.hykin@gmail.com>
2017-08-17 11:28:37 -0500

    Created Snake version 2

--- run.py
+++ run.py
@@ -5,75 +5,64 @@ import webbrowser
 
 
 
-# gets the current directory and removes the newline from the end
+# gets the current directory and removes the newline 
 directory_ = str(sh.pwd())[0:-1]
 
 # list of files/folders
 command_output = str(sh.ls('-1'))
 file_names = re.findall(r'(.+)\n',command_output)
 
-# check the directory, make sure its in the Mac's apache server directory
+# check the directory 
 if re.match(r'/Library/WebServer/Documents/', directory_) == None:
     print "\n\n\n\nI don't think you're in the right directory"
     print "to run web stuff, the project needs to be in:"
     print "Library/WebServer/Documents"
     exit(1)
     
-# figure out/create the url based on the current directory
+# figure out the url    
 remaining_dir = re.sub(r'^/Library/WebServer/Documents/','',directory_)
 url_ = 'http://localhost/' + remaining_dir + '/index.html'
 
 
 # make sure apache_is_running 
 if True:
-    apache_is_running = False
     # list all the processes, only show the command column, convert them to a big string 
     all_processes = str(sh.ps('ax', '-o', 'command'))
     # split up the string into a list of lines 
     all_processes = all_processes.splitlines()
-    # check each line for /usr/sbin/httpd 
-        # Note:
-        # this isn't foolproof but for the most part 
-        # only the apache process will include /usr/sbin/httpd in its command
+    apache_is_running = False
+    # check each line for /usr/sbin/httpd
     for each in all_processes:
-        # if you find it, then apache is probably running
         if re.search(r'/usr/sbin/httpd', each):
             apache_is_running = True
-    
-    # if apache is not running, then try to start it
     if not apache_is_running:
-        print "\n\n\n\nHey I think apache isn't running"
-        print "It might take a second to start "
-        print "and it can be started from here with your password"
+        #print "\n\n\n\nHey I think apache isn't running"
+        #print "It might take a second to start "
+        #print "and it can be started from here with your password"
         os.system('sudo apachectl start')
 
 
 
-# convert .pug and .sass files to .html and .css
+# process pug and sass
 if True:
     # check for pug file
     if True:
-        # if no pug file, then give an error 
         if 'index.pug' not in file_names:
                 print "\n\n\n\nI'm looking for an index.pug file but I don't see one"
                 print "to fix this, either dont use this run.py or create an index.pug"
                 exit(1)
-        # otherwise convert the pug file
         else:
             os.system("pug -P index.pug")
     # check for sass file 
     if True:
-        # if no sass file, then give an error 
         if 'main.sass' not in file_names:
             print "\n\n\n\nI'm looking for an main.sass file but I don't see one"
             print "to fix this, either dont use this run.py or create an main.sass"
             exit(1)
-        # otherwise convert the sass file
         else:
             os.system("sass main.sass main.css")
 
 
 # open the url in the default browser
 webbrowser.open(url_)
-# print the url for copy-pasting to other browsers
 print url_
--- sketch.js
+++ sketch.js
@@ -1,6 +1,14 @@
-// things to add 
-    // game over screen
-    // intro screen
+// FIXME, issue of clicking-out-of-window then releasing key then clicking back into the window
+
+// TODO, comment on the changes, upload to github
+// TODO, full game over screen
+// TODO, full pause screen
+// TODO, back button from canvas
+// TODO, better looking info page
+// TODO, better looking snake-canvas and score
+// TODO, package this up into an electron app 
+
+
 
 
 // helper functions
@@ -8,13 +16,20 @@ function getRandomInt(min_included, max_not_included)
     {
         return Math.floor(Math.random() * (max_not_included - min_included)) + min_included;
     }
+function sigmoid(input_) 
+    {
+        return 1/(1+Math.pow(Math.E, -input_));
+    }
 
-// global vars
+
+// global vars (mostly for the can)
 var SCORE             = 0;
 var DEFAULT_FRAMERATE = 10;
 var FRAMERATE         = DEFAULT_FRAMERATE;
 var PIXELS_PER_BLOCK  = 30;
 var INFOBAR_HEIGHT    = 60;
+var REL_WIDTH  = window.innerWidth;
+var REL_HEIGHT = window.innerHeight;
 var NUM_OF_HORIZONTAL_BLOCKS = Math.floor( window.innerWidth  / PIXELS_PER_BLOCK);
 var NUM_OF_VERTICAL_BLOCKS   = Math.floor((window.innerHeight - INFOBAR_HEIGHT) / PIXELS_PER_BLOCK);
 var CENTER_BLOCK_X           = Math.round(NUM_OF_HORIZONTAL_BLOCKS/2)*PIXELS_PER_BLOCK;
@@ -39,11 +54,17 @@ function Block(x=0,y=0)
                 this.x = new_x;
                 this.y = new_y;
             }
-        this.Draw = function (input_={color_:color(255)}) 
+        this.Draw = function (color_=color(255),border_=color('#4CAF50')) 
             {
                 // summary:
                     // this will draw the block on the canvas
-                fill(input_.color_);
+                if (border_ == null)
+                    {
+                        border_ = color(255,0)
+                    }
+                fill(color_)
+                stroke(border_)
+                strokeWeight(2)
                 rect(this.x, this.y, PIXELS_PER_BLOCK, PIXELS_PER_BLOCK);
             }
 
@@ -73,8 +94,28 @@ function KillerBlock(x=0,y=0)
         this.x      = this.block.x;
         this.y      = this.block.y;
         this.JumpTo = this.block.JumpTo;
-        this.Draw   = this.block.Draw; 
+        this.Draw   = function(color_=color(255),border_=color(150))
+            {
+                this.block.Draw(color_,border_)
+            }
+        
     }
+function TailBlock(x,y)
+    {
+        this.block = new KillerBlock(x,y)
+        this.Draw = function()
+            {
+                this.block.Draw(220,255)
+            }
+        this.Delete = function()
+            {
+                this.block.Delete()
+            }
+    }
+
+
+
+
 
 // global objects 
 var BORDER    = {
@@ -109,7 +150,7 @@ var BORDER    = {
                 // right column (doesn't do the last one)
                 for(loop_number = 1; loop_number <= NUM_OF_VERTICAL_BLOCKS-1; loop_number++)
                     {
-                        this.blocks.add(     new KillerBlock(  width-PIXELS_PER_BLOCK, (loop_number-1)*PIXELS_PER_BLOCK  )    );
+                        this.blocks.add(     new KillerBlock(  (NUM_OF_HORIZONTAL_BLOCKS-1) * PIXELS_PER_BLOCK, (loop_number-1)*PIXELS_PER_BLOCK  )    );
                     }
             
                 // left column (doesn't do the first one)
@@ -146,12 +187,13 @@ var THE_FOOD  = {
         y : getRandomInt(1, NUM_OF_VERTICAL_BLOCKS  -1) * PIXELS_PER_BLOCK,
         
         // methods
-        Update : function (input_={ color_:color(255,0,100) }) 
+        Update : function (input_={ border_:color(255,0,100), color_:color(255,0) }) 
             {
                 // summary
                     // this will Draw the food block onto the screen
-                fill(input_.color_);
-                rect(this.x, this.y, PIXELS_PER_BLOCK, PIXELS_PER_BLOCK);
+                stroke(input_.border_)
+                fill(input_.color_)
+                rect(this.x, this.y, PIXELS_PER_BLOCK, PIXELS_PER_BLOCK)
             },
         ChangeLocation : function () 
             {
@@ -168,14 +210,17 @@ var THE_SNAKE = {
             // it calls THE_FOOD.ChangeLocation() 
             // GameOver resets the snake 
         
+
         // the head is just a regular block 
         head       : new Block( CENTER_BLOCK_X, CENTER_BLOCK_Y ),
         // the tail will contain KillerBlocks 
         tail       : [],
         // the direction will be 1 of 5 values, null, 'up' , 'down' , 'left' , 'right' 
         direction  : null,
-        // when grow is true, the tail will be extended by 1 block on the next update
-        grow       : false,
+        // when grow is >1, the tail will be extended by 1 block on the next update
+        grow       : 0,
+        // last time it ate a block
+        last_eat : 1,
         // direction_buffer keeps track of the next few directions for THE_SNAKE, see ReceiveDirection
         direction_buffer: [],
         
@@ -218,7 +263,7 @@ var THE_SNAKE = {
                     // if this.grow is true, the last block isn't deleted
                 
                 // create a new block at same x/y as the head
-                this.tail.push(new KillerBlock(this.head.x, this.head.y));
+                this.tail.push(new TailBlock(this.head.x, this.head.y));
                 
                 // then move the head
                      if (this.direction == 'up') { this.head.y = this.head.y - PIXELS_PER_BLOCK; }
@@ -226,15 +271,17 @@ var THE_SNAKE = {
                 else if (this.direction == 'left') { this.head.x = this.head.x - PIXELS_PER_BLOCK; }
                 else if (this.direction == 'right') { this.head.x = this.head.x + PIXELS_PER_BLOCK; }
                 
-                // delete old block if grow is false
-                if (this.grow)             
+                // delete old block if grow is < 1
+                if (this.grow >= 1)             
                     { 
-                        this.grow = false;     
+                        this.grow -= 1
+                        this.last_eat = 1
                     }
                 else if (this.tail.length != 0) 
                     { 
-                        this.tail[0].Delete(); 
-                        this.tail.splice(0,1); 
+                        this.last_eat += 1
+                        this.tail[0].Delete()
+                        this.tail.splice(0,1)
                     }
             },
         Is__DirectionValidGiven__Direction : function(future_direction, past_direction)
@@ -341,11 +388,11 @@ var THE_SNAKE = {
                             this.Move();
                         }
                 // Draw the snake 
-                    this.head.Draw();
                     for (each of this.tail) 
                         {
-                            each.Draw();
+                            each.Draw()
                         }
+                    this.head.Draw();
                 // check for collisions
                 this.CollisionChecker();
             },
@@ -357,20 +404,60 @@ var THE_SNAKE = {
                 // delete each KillerBlock in the tail
                 for (each of this.tail) 
                     {
-                        each.Delete(); 
-                        delete each;
+                        each.Delete() 
+                        delete each
                     }
                 // reset the tail 
-                this.tail = [];
+                this.tail = []
                 // reset the location of the head
                 delete this.head;
                 this.head = new Block( CENTER_BLOCK_X, CENTER_BLOCK_Y );
                 // reset the direction 
-                this.direction = null;
+                this.direction = null
+                // clear the direction_buffer
+                this.direction_buffer = []
             }
     } // end THE_SNAKE 
 
 // functions
+function Align(way_to_align,html_element)
+    {
+             if (way_to_align == 'vertically'  )
+                {
+                    // create outer 
+                    outer_Div = createDiv('')
+                    outer_Div.addClass('outer_part_CenterVertically')
+                    // create middle
+                    middle_Div = createDiv('')
+                    middle_Div.addClass('middle_part_CenterVertically')
+                    outer_Div.child(middle_Div)
+                    // make input the child
+                    middle_Div.child(html_element)
+                    // return outer encase it needs to be moved
+                    output_Div = outer_Div
+                }
+        else if (way_to_align == 'horizontally')
+            {
+                // create a centeringDiv 
+                centeringDiv = createDiv('')
+                centeringDiv.addClass('outer_part_CenterHorizantally')
+                centeringDiv.child(html_element)
+                html_element.style('display','inline-block')
+                output_Div = centeringDiv
+            }
+        else 
+            {
+                // create a centeringDiv 
+                holdingDiv = createDiv('')
+                holdingDiv.style('text-align', way_to_align)
+                holdingDiv.child(html_element)
+                html_element.style('display','inline-block')
+                output_Div = holdingDiv
+            }
+        
+        // inherit the html_element's height and width
+        return output_Div
+    }
 function GameOver()
     {
         // summary:
@@ -391,29 +478,159 @@ function Pause()
             // just pauses the screen
         alert('Paused');
     }
-function ShowScore()
+function DrawScore()
     {
         // summary
             // this just puts the score in the bottom left corner
             // this should be called every frame
-        textSize(30);
-        text("Score: " +SCORE, 10, (NUM_OF_VERTICAL_BLOCKS * PIXELS_PER_BLOCK) + 35);
-        fill(0, 102, 153);
+        textSize(30)
+        fill(255,0,100)
+        stroke(0,0)
+        text("Score: " +SCORE, 10, (NUM_OF_VERTICAL_BLOCKS * PIXELS_PER_BLOCK) + 35)
+        
     }
+function ShowIntro() 
+    {
+        // Create the intro screen
+        intro_Div = createDiv('')
+        intro_Div.style('width','100%')
+        intro_Div.style('margin-bottom','20%')
+        intro_page_Div = Align('horizontally',intro_Div)
+        intro_page_Div.style('width','100%')
+        
+        // Title
+        title_P = createP('Snake!')
+        title_P.style('font-size','800%')
+        title_P.style('font-family','Helvetica-Light')
+        title_P_holder_Div = Align('horizontally',title_P)
+        title_P_holder_Div.parent(intro_Div)
+        
+
+        
+        // Create the start_Button button
+        start_Button = createButton('Start')
+        start_Button.addClass('introButton')
+        start_Button.mousePressed(FromIntroToStartGame)
+        start_Button.style('width',"28%")
+        intro_Div.child(Align('horizontally',start_Button))
+        function FromIntroToStartGame() { intro_page_Div.remove(); StartGame(); }
+        
+        // Create the controls button
+        controls_Button = createButton('Controls')
+        controls_Button.addClass('introButton')
+        controls_Button.mousePressed(FromIntroToShowControls)
+        controls_Button.style('width',"28%")
+        intro_Div.child(Align('horizontally',controls_Button))
+        function FromIntroToShowControls() { intro_page_Div.remove(); ShowControls(); }
+        // Create the info button 
+        info_Button = createButton('Info')
+        info_Button.addClass('introButton')
+        info_Button.mousePressed(FromIntroToShowInfo)
+        info_Button.style('width',"28%")
+        intro_Div.child(Align('horizontally',info_Button))
+        function FromIntroToShowInfo() { intro_page_Div.remove(); ShowInfo(); }
+    }
+function StartGame() 
+    {
+        // set the frameRate
+        frameRate(FRAMERATE)
+        // show the canvas, 
+        canvas.show()
+        // Create the border 
+        BORDER.Create()
+    }
+function ShowControls() 
+    {
+        // Create the controls container 
+        controls_page_Div = createDiv('')
+        controls_page_Div.style('margin-bottom','20%')
+        controls_Div      = createDiv('')
+        controls_Div.parent(controls_page_Div)
+        controls_Div.addClass('grid')
+        //CenterHorizantally(keys_Div).style('margin-top',REL_HEIGHT*0.25 +"px")
+        function CreateKeyExplation(key_,explaination_) 
+            {
+                 
+                key_Div = createDiv(key_)
+                key_Div.addClass('key')
+                controls_Div.child(Align('right',key_Div))
+                
+                explaination_Div = createDiv(explaination_)
+                explaination_Div.addClass('key_explaination')
+                controls_Div.child(Align('vertically',explaination_Div))
+            }
+        // Arrow keys 
+        CreateKeyExplation('Arrow Keys','Change Direction')
+        //controls_Div.child(createDiv('Arrow Keys'))
+        //controls_Div.child(createDiv('blah blah blah'))
+        // speed up
+        CreateKeyExplation('W','Speeds Up!')
+        //controls_Div.child(createDiv('W'))
+        //controls_Div.child(createDiv('Speeds Up!'))
+        // slow down
+        CreateKeyExplation('Shift','S l o w s  D o w n')
+        //controls_Div.child(createDiv())
+        //controls_Div.child(createDiv())
+        // Pause
+        CreateKeyExplation('Esc or Spacebar','Pauses')
+        //controls_Div.child(createDiv('Esc or Spacebar'))
+        //controls_Div.child(createDiv('Pause'))
+
+        back_Button = createButton('Back')
+        back_Button.addClass('introButton')
+        back_Button.mousePressed(GoToShowIntro)
+        back_Button.style('width','28%')
+        controls_page_Div.child(Align('horizontally',back_Button))
+        
+        
+        function GoToShowIntro() 
+            {
+                controls_page_Div.remove()
+                ShowIntro()
+            }
+            
+        
+    }
+function ShowInfo() 
+    {
+        // create page div 
+        info_page_Div = createDiv('')
+        
+        // create info text
+        actual_info1_P = createP("Developed by Jeff Hykin<br>")
+        info_page_Div.child(Align('horizontally',actual_info1_P))
+        
+        var a_string = (function () {/*
+           This was programmed using p5.js, which I learned from Daniel Shiffman's tutorials on Youtube.
+        */}).toString().match(/[^]*\/\*([^]*)\*\/\}$/)[1];
+        actual_info2_P = createP(a_string)
+        actual_info2_P.style('font-size','18px')
+        actual_info2_P.style('margin-bottom','10%')
+        info_page_Div.child(Align('horizontally',actual_info2_P))
+        
+        // create back button
+        back_Button = createButton('Back')
+        back_Button.addClass('introButton')
+        back_Button.mousePressed(FromInfoToShowIntro)
+        back_Button.style('width','28%')
+        back_Button_holder_Div = Align('horizontally',back_Button)
+        info_page_Div.child(back_Button_holder_Div)
+        function FromInfoToShowIntro() 
+            {
+                info_page_Div.remove()
+                ShowIntro()
+            }
+    }
+
 
 // setup is a builtin with p5.js, 
 // it is run once 
 function setup() 
     {
-        // set the frameRate
-        frameRate(FRAMERATE);
-        // create the canvas, 
-        // note:
-            // the -5 is because there is an annoying gap 
-            // that causes the page to shift with the up/down arrow keys 
-        createCanvas(window.innerWidth, window.innerHeight-5);
-        // Create the border 
-        BORDER.Create();
+        // Hide the canvas (otherwise a default one is created because of p5)
+        canvas = createCanvas(window.innerWidth, window.innerHeight)
+        canvas.hide()
+        ShowIntro()
     }
 
 // Draw is a builtin with p5.js, 
@@ -421,15 +638,16 @@ function setup()
 function draw() 
     {
         // draw the background
-        background(51);
-        // draw the border
-        BORDER.Update();
+        background(255);
+
         // update the snake
         THE_SNAKE.Update();
+        // draw the border
+        BORDER.Update();
         // update the food
         THE_FOOD.Update();
         // show score 
-        ShowScore();
+        DrawScore();
     }
 
 // keyPressed is a builtin with p5.js 
@@ -449,7 +667,7 @@ function keyPressed()
         else if (key     === 'W'   ) { FRAMERATE = FRAMERATE * 2; frameRate(FRAMERATE); }
         else if (keyCode === SHIFT ) { FRAMERATE = FRAMERATE / 2; frameRate(FRAMERATE); }
     }
-    
+
 // keyReleased is a builtin with p5.js 
 // it is run every time a key is released
 function keyReleased()
